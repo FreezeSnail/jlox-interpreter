1c7e6ecb738b65a790ba8b09db9cee72f4ea397b
FreezeSnail <36997863+FreezeSnail@users.noreply.github.com>
2019-12-31 11:14:14 -0500

    Add files via upload

---
+++ lox/AstPrinter.java
@@ -0,0 +1,77 @@
+package com.craftinginterpreters.lox;
+
+import com.craftinginterpreters.lox.Expr.Assign;
+import com.craftinginterpreters.lox.Expr.Binary;
+import com.craftinginterpreters.lox.Expr.Call;
+import com.craftinginterpreters.lox.Expr.Grouping;
+import com.craftinginterpreters.lox.Expr.Literal;
+import com.craftinginterpreters.lox.Expr.Logical;
+import com.craftinginterpreters.lox.Expr.Unary;
+import com.craftinginterpreters.lox.Expr.Variable;
+
+// Creates an unambiguous, if ugly, string representation of AST nodes.
+class AstPrinter implements Expr.Visitor<String> {                     
+  String print(Expr expr) {                                            
+    return expr.accept(this);                                          
+  }
+
+  @Override                                                          
+  public String visitBinaryExpr(Expr.Binary expr) {                  
+    return parenthesize(expr.operator.lexeme, expr.left, expr.right);
+  }
+
+  @Override                                                          
+  public String visitGroupingExpr(Expr.Grouping expr) {              
+    return parenthesize("group", expr.expression);                   
+  }                                                                  
+
+  @Override                                                          
+  public String visitLiteralExpr(Expr.Literal expr) {                
+    if (expr.value == null) return "nil";                            
+    return expr.value.toString();                                    
+  }                                                                  
+
+  @Override                                                          
+  public String visitUnaryExpr(Expr.Unary expr) {                    
+    return parenthesize(expr.operator.lexeme, expr.right);           
+  }   
+  
+  private String parenthesize(String name, Expr... exprs) {
+	  StringBuilder builder = new StringBuilder();
+
+	    builder.append("(").append(name);                      
+	    for (Expr expr : exprs) {                              
+	      builder.append(" ");                                 
+	      builder.append(expr.accept(this));                   
+	    }                                                      
+	    builder.append(")");                                   
+
+	    return builder.toString();                             
+	  }
+
+@Override
+public String visitVariableExpr(Variable expr) {
+	// TODO Auto-generated method stub
+	return null;
+}
+
+@Override
+public String visitAssignExpr(Assign expr) {
+	// TODO Auto-generated method stub
+	return null;
+}
+
+@Override
+public String visitLogicalExpr(Logical expr) {
+	// TODO Auto-generated method stub
+	return null;
+}
+
+@Override
+public String visitCallExpr(Call expr) {
+	// TODO Auto-generated method stub
+	return null;
+}  
+  
+}
+           
